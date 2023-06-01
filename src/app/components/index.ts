91b083b0fcbc30235a1ebb4db769eb43b36b559e
Vinicius Italo <viniciuscruz@frwk.com.br>
2023-06-01 02:10:00 -0300

    refactor: implement exports all components and import in Home the cart

---
+++ src/app/components/index.ts
@@ -0,0 +1,3 @@
+export * from "./Cart";
+export * from "./Button";
+export * from "./CardSmall";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { CartComponent } from "./components";
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <CartComponent />
     </main>
   )
 }
